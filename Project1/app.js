c5eb88eda4510a8c147f2909eeaf00a5afda2f89
achalofficial <achal.official@gmail.com>
2020-02-28 12:36:27 +0530

    var < let < const

---
+++ Project1/app.js
@@ -0,0 +1,50 @@
+// INTRO TO JAVASCRIPT 
+// LECTURE 1
+
+// Single Line Comment
+
+/* 
+Multi Line 
+Comment
+*/
+
+// Log Message
+console.log("Hello World");
+
+// Log Variable
+var greeting = "Hola !!";
+console.log(greeting);
+
+// Log Integer
+console.log(123);
+
+// Log Array
+console.log([1,2,3,4,5,6]);
+
+// Log Objects
+console.log({a:1 , b:2});
+
+//Log In Table Form
+console.table({a:1 , b:2});
+
+//Log Error 
+console.error("This is an Error");
+
+//Log Clear
+console.clear();
+
+//Log Warning
+console.warn("This is a Warning !!")
+
+//Log Total Time
+console.time("Hola");
+
+  console.log("Hello World");
+  console.log("Hello World");
+  console.log("Hello World");
+  console.log("Hello World");
+  console.log("Hello World");
+  console.log("Hello World");
+
+//End Logging time
+console.timeEnd("Hola");
---
+++ Project1/app_2.js
@@ -0,0 +1,39 @@
+// Declear variable using VAR keyword
+
+var name = "Zapp";
+console.log(name);
+
+var greeting ;
+console.log(greeting);
+greeting = "John";
+console.log(greeting);
+
+// A Variable in Javascript can incloude only numbers , letters , _ and $ sign
+// They Cant Start with a number.
+// multiname Varables
+
+var firstName = "Johnson"; // camel case
+var first_name = "sara"; // underscore
+var FirstName = "Son"; // Pascal Case
+
+console.log(firstName);
+console.log(first_name);
+console.log(FirstName);
+
+
+// Declear using the LET Keyword
+
+console.clear(); // To clear the log 
+let fName = "Jaszz";
+console.log(fName);
+
+// Declearation using the CONST Keyword
+
+/*
++ We cant reassign the const value
++ We have to assign the Value
+*/
+
+const Fname = "Johnny";
+console.log(Fname);
+Fname = "Jaanu"; // Produce an Error
