f1ad6e5829b70bc9fc701e808ed4ebde6104ff3c
achalofficial <achal.official@gmail.com>
2020-02-29 00:48:37 +0530

    Working on Type Conversion

---
+++ Fundamentals/app_4.js
@@ -0,0 +1,26 @@
+// TYPE CONVERSION
+
+let val ;
+
+// Number to String
+val = String(555) ;
+  // For Expression
+val = String(4+4);
+
+// Boolean to String
+val = String(true);
+
+// Date to string
+val = String( new Date());
+
+// Array to String
+val = String([1,2,3,4])
+
+// toString()
+val = (55).toString();
+
+
+// Output
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
