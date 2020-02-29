e1cd1c85b7b7cec96295e669fd06d628b7d3ae39
achalofficial <achal.official@gmail.com>
2020-02-29 20:15:47 +0530

    Type Conversion Finish

--- Fundamentals/app_4.js
+++ Fundamentals/app_4.js
@@ -1,26 +1,50 @@
 // TYPE CONVERSION
 
-let val ;
+let val;
 
 // Number to String
-val = String(555) ;
-  // For Expression
-val = String(4+4);
+val = String(555);
+// For Expression
+val = String(4 + 4);
 
 // Boolean to String
 val = String(true);
 
 // Date to string
-val = String( new Date());
+val = String(new Date());
 
 // Array to String
-val = String([1,2,3,4])
+val = String([1, 2, 3, 4])
 
 // toString()
 val = (55).toString();
 
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(null);
+val = Number('hello')
+val = Number([1,2,3,4,5]);
+// NaN is given when it is not possible conver to number
 
-// Output
-console.log(val);
-console.log(typeof val);
-console.log(val.length);
+// parseInt()
+val = parseInt('100');
+val = parseInt('100.30'); // Still get 100 since it will convert it int type
+val = parseFloat('100.3'); // When you want to return Float Point
+
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length); // gives length of the given string
+// console.log(val.toFixed()); // Allow to put decimal to numbers
+
+
+// Exlict Type Conversion
+const val1 = String('5') ;
+const val2 = 6 ;
+const sum = Number(val1) + val2 ;
+
+console.log(sum);
+console.log(typeof sum);
+
+
