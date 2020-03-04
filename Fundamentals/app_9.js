92e3078c683bffc0c135172009ef362d310ce43e
achalofficial <achal.official@gmail.com>
2020-03-04 17:32:25 +0530

    Object Literals

--- Fundamentals/app_9.js
+++ Fundamentals/app_9.js
@@ -1,1 +1,29 @@
 // Object Literals
+
+const person = {
+  firstName:'Steve',
+  lastName :'Smith',
+  age: 30,
+  email: 'steve@gmail.com',
+  hobbies:['music','games'],
+  address:{
+    city: 'NY',
+    state:'LA',
+  },
+  getbirthyear: function(){
+    return 1987;
+  }
+
+}
+
+let val ;
+
+val = person ;
+
+// get specific Value
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+
+console.log(val);
+
