880f7a8b028e941ed500ba749b89e2f85426d525
achalofficial <achal.official@gmail.com>
2020-03-04 22:16:24 +0530

    Object Literals

--- Fundamentals/app_9.js
+++ Fundamentals/app_9.js
@@ -11,9 +11,13 @@ const person = {
     state:'LA',
   },
   getbirthyear: function(){
-    return 1987;
+    //return 1987 - age ; 
+    /*
+        + This will give an error, as the varible 'age' is not available here
+        + to use the variable decleared inside the class, we need to use the 'This' keyword. By using the 'THis' Keyword we extend the scope of the varible. 
+    */
+   return 1987 - this.age;
   }
-
 }
 
 let val ;
@@ -24,6 +28,10 @@ val = person ;
 val = person.firstName;
 val = person['lastName'];
 val = person.age;
+val = person.hobbies;
+val = person.address['city'];
+val = person.getbirthyear();
+
 
 console.log(val);
 
