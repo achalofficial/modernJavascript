dded58d17bad0d8415c9f80a260b51ff67b15a0d
achalofficial <achal.official@gmail.com>
2020-03-07 02:04:40 +0530

    Switched Done

--- Fundamentals/app_12.js
+++ Fundamentals/app_12.js
@@ -1,1 +1,14 @@
 // Switches
+
+const color = 'red';
+
+switch(color){
+  case 'red':
+    console.log('Color is Red');
+    break ;
+  case 'Blue':
+    console.log('Color is Blue');
+  default :
+    console.log('Color is neither Red nor Blue');
+    break ;
+}
