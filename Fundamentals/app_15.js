3ca0edcc795879196a096673ccc29249bd9e355c
achalofficial <achal.official@gmail.com>
2020-03-09 19:43:00 +0530

    Window Objct Done

--- Fundamentals/app_15.js
+++ Fundamentals/app_15.js
@@ -1,1 +1,55 @@
-// A look at window object
+// A look at Window Object
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// ALERT
+//window.alert('HEllo World');
+//alert('Without Window');
+
+// PROMPT
+//const input = prompt();
+//alert(input);
+
+//Confirm
+// if(confirm('Are you sure ?')){
+//   console.log('Deleted');
+// }
+
+// Properties
+let val ;
+
+//Outer Height and Width
+val = window.outerHeight;
+val = window.outerWidth;
+
+//INner Height and Width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll Points
+val = window.scrollY; // Vertical Axis
+val = window.scrollX; // Horizontal Axis
+
+// Location Object
+val = window.location;
+val = window.location.host;
+val = window.location.pathname;
+val = window.location.port;
+val = window.location.hash;
+
+// Redirect
+//window.location.href = 'http://google.com';
+//Reload
+//window.location.reload();
+//HIstory
+// window.history.go(-1); // for specific Page
+// val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+console.log(val);
