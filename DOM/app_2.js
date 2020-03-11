52818f9a718973e216c54de8026daa702ac722d0
achalofficial <achal.official@gmail.com>
2020-03-11 23:35:23 +0530

    Selector for Single Element finished

--- DOM/app_2.js
+++ DOM/app_2.js
@@ -25,5 +25,26 @@ document.getElementById('task-title').innertext = 'Task List 2';
 
 const taskTitle = document.getElementById('task-title');
 
-taskTitle.textContent = 'Task List ( Variable Use)';
+//taskTitle.textContent = 'Task List ( Variable Use)';
 
+// document.query Selector()
+/*
+    + This is used as JQuery Selector
+    + We can select Elements by ID or Class or element type ( H5 )
+    + if there are many elements of the same type then the first element is taken care into.
+    + We can also do sub classes and nested classes or elements
+    + to select the class in between, we can use CSS3 Sub classes like last-child and nth-child(<number>)
+    + for the nth-child: we can do odd and even too but only first odd and first even
+*/
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'green';
+
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
