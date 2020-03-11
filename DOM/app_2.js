5bcfc6c9513924b494064624adf515a72c31aeb1
achalofficial <achal.official@gmail.com>
2020-03-11 23:16:13 +0530

    Selector for Single Element

--- DOM/app_2.js
+++ DOM/app_2.js
@@ -11,3 +11,19 @@ console.log(document.getElementById('task-title').id);
 
 //IF you wish to hide something from the document
 //document.getElementById('task-title').style.display = 'none';
+
+// Change the Content
+document.getElementById('task-title').textContent = 'Task List';
+document.getElementById('task-title').innertext = 'Task List 2';
+//document.getElementById('task-title').innerHTML = "<h5>Inner HTML Inserted</h5>";
+
+/*
+    + Doing getElementby ID is not efficient 
+    + We find another way of doing this
+    + We declear a variable and use that variable everywhere
+*/
+
+const taskTitle = document.getElementById('task-title');
+
+taskTitle.textContent = 'Task List ( Variable Use)';
+
