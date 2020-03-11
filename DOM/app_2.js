9e6d86d2baefbdff6be22fb8c1d362ad15a7e064
achalofficial <achal.official@gmail.com>
2020-03-11 23:02:39 +0530

    Selector for Single Element

--- DOM/app_2.js
+++ DOM/app_2.js
@@ -1,2 +1,13 @@
 // DOM Selectors
 
+// get element by ID
+console.log(document.getElementById('task-title'))
+
+//get things from the elements
+console.log(document.getElementById('task-title').id);
+
+//change styling 
+//document.getElementById('task-title').style.backgroundColor = '#224466';
+
+//IF you wish to hide something from the document
+//document.getElementById('task-title').style.display = 'none';
