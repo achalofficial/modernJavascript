a6c572e9d2820c113b6af4bbce45e43e20b43d87
achalofficial <achal.official@gmail.com>
2020-03-12 23:27:28 +0530

    Query Selector Multiple Elements

--- DOM/app_3.js
+++ DOM/app_3.js
@@ -1,2 +1,17 @@
 // Query Selector for Multiple Elements
+// document.getElementsBy ClassName
+
+const items = document.getElementsByClassName('collection-item');
+
+console.log(items);
+console.log(items[0]);
+
+items[0].style.color = 'red';
+items[3].textContent = 'Hello';
+
+// We can attach the getElemetsByClassName to elements rather than adding it globally
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+console.log(listItems);
+
 
