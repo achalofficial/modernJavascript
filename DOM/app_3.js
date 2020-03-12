4343caef284eaf4d00eed2af80389509e1e73237
achalofficial <achal.official@gmail.com>
2020-03-12 23:42:38 +0530

    Query Selector Multiple Elements

--- DOM/app_3.js
+++ DOM/app_3.js
@@ -1,36 +1,39 @@
-// Query Selector for Multiple Elements
-// document.getElementsBy ClassName
+// // Query Selector for Multiple Elements
+// // document.getElementsBy ClassName
 
-// const items = document.getElementsByClassName('collection-item');
+// // const items = document.getElementsByClassName('collection-item');
+
+// // console.log(items);
+// // console.log(items[0]);
+
+// // items[0].style.color = 'red';
+// // items[3].textContent = 'Hello';
+
+// // We can attach the getElemetsByClassName to elements rather than adding it globally
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// console.log(listItems);
+
+// //document.getElementByTagname
+// let items = document.getElementsByTagName('li');
 
 // console.log(items);
 // console.log(items[0]);
-
 // items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
+// items[3].textContent = 'Hello'
 
-// We can attach the getElemetsByClassName to elements rather than adding it globally
-const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// // Convert the HTML Collection into array
+// items = Array.from(items);
 
-console.log(listItems);
+// //items.reverse();
 
-//document.getElementByTagname
-let items = document.getElementsByTagName('li');
+// items.forEach(function(item, index){
+//   console.log(item)
+//   item.textContent = `${index}: Hello`;
 
-console.log(items);
-console.log(items[0]);
-items[0].style.color = 'red';
-items[3].textContent = 'Hello'
+// });
 
-// Convert the HTML Collection into array
-items = Array.from(items);
+// console.log(items);
 
-//items.reverse();
-
-items.forEach(function(item, index){
-  console.log(item)
-  item.textContent = `${index}: Hello`;
-
-});
-
-console.log(items);
+// Query Selector All
+const items = document.querySelectorAll('ul.collection li.collection')
