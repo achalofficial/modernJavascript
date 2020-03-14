1391dff4fb4321ce66aab0aff59eb1f0e26e73ad
achalofficial <achal.official@gmail.com>
2020-03-14 22:18:09 +0530

    Traversing the DOM

--- DOM/app_4.js
+++ DOM/app_4.js
@@ -1,3 +1,14 @@
 // Traversing the DOM
 
+let val ;
+
+const list = document.querySelector('ul.collection');
+const listItems = document.querySelector('li.collection-item:first-child');
+
+val = list ;
+val = listItems ;
+
+
+
+console.log(val);
 
