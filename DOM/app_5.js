d53642cff603af373d4074c39b1879023890c14c
achalofficial <achal.official@gmail.com>
2020-03-15 23:28:32 +0530

    Create Element in DOM

--- DOM/app_5.js
+++ DOM/app_5.js
@@ -1,1 +1,18 @@
 // Creating the Elements
+
+const li = document.createElement('li');
+
+//Add Class
+li.className = 'collection-item';
+
+//Add ID
+li.id = 'new-item';
+
+//Add Attribute
+li.setAttribute('title','New Item');
+
+//Create Text Node and Append
+
+
+
+console.log(li);
