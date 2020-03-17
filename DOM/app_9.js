e0ac328b97007177fa9529d7147032d97ed20a34
achalofficial <achal.official@gmail.com>
2020-03-17 22:40:38 +0530

    Keyboard and Input Events

---
+++ DOM/app_9.js
@@ -0,0 +1,34 @@
+// Keyboard and Input Events
+
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+
+//to clear the input section
+taskInput.value = '';
+
+//form.addEventListener('submit', runEvent);
+
+// // Key Down
+// taskInput.addEventListener('keydown', runEvent);
+
+
+// Key Up
+taskInput.addEventListener('keyup', runEvent);
+
+// key Press
+taskInput.addEventListener('keypress', runEvent);
+
+
+function runEvent(e) {
+  console.log(`EVENT TYPE : ${e.type}`);
+
+  //console.log(e.target.value);
+  
+  //heading.innerText = e.target.value ;
+
+  // Get Input Value
+  //console.log(taskInput.value);
+  // To prevent default behaviour
+  //e.preventDefault();
+}
