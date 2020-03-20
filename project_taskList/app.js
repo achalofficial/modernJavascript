9798ff4a079ffd422c10344871f72a2a60374861
achalofficial <achal.official@gmail.com>
2020-03-21 01:10:16 +0530

    Project Done Presist to Local Storage Starting

--- project_taskList/app.js
+++ project_taskList/app.js
@@ -12,8 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
-  // Remove task Event
+  // Remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -37,8 +41,6 @@ function addTask(e) {
   // Append the link to li
   li.appendChild(link);
 
-  console.log(li); 
-  
   // Append li to ul
   taskList.appendChild(li);
 
@@ -50,8 +52,35 @@ function addTask(e) {
 
 // Remove Task
 function removeTask(e) {
-  
-  
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
 
-  e.preventDefault();
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
