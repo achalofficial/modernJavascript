8aad4d24f0a76d405356d0e0131f3e73b0fead9d
achalofficial <achal.official@gmail.com>
2020-03-20 22:34:38 +0530

    Task List Error

--- project_taskList/app.js
+++ project_taskList/app.js
@@ -1,1 +1,43 @@
-// Writing the Script
+// Define our UI Variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load All Event Listeners
+loadEventListeners();
+
+// Load all Event Listeners
+function loadEventListeners(){
+  // add Task Events
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {  
+  if(taskInput.value === ''){
+    alert('Add a Task');
+  }
+
+  // Create a Li Element 
+  const li = document.createElement('li');
+  // Add Class
+  li.className = 'collection-item';
+  //text node and append to the li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add a Class
+  link.className = 'delete-item secondary-content';
+  // Icon HTML
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Appent to li
+  li.appendChild(link);
+
+  console.log(li);
+  
+
+  e.preventDefault();
+}
