ae7fe72f6768d3384ccce17547033fd0375455ee
achalofficial <achal.official@gmail.com>
2020-03-20 22:38:57 +0530

    Task List UI Ready

--- project_taskList/app.js
+++ project_taskList/app.js
@@ -1,43 +1,47 @@
-// Define our UI Variables
-
+// Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load All Event Listeners
+// Load all event listeners
 loadEventListeners();
 
-// Load all Event Listeners
-function loadEventListeners(){
-  // add Task Events
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
   form.addEventListener('submit', addTask);
 }
 
 // Add Task
-function addTask(e) {  
-  if(taskInput.value === ''){
-    alert('Add a Task');
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
   }
 
-  // Create a Li Element 
+  // Create li element
   const li = document.createElement('li');
-  // Add Class
+  // Add class
   li.className = 'collection-item';
-  //text node and append to the li
+  // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
-  // Add a Class
+  // Add class
   link.className = 'delete-item secondary-content';
-  // Icon HTML
+  // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Appent to li
+  // Append the link to li
   li.appendChild(link);
 
   console.log(li);
   
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
 
   e.preventDefault();
 }
