e10191e5839c52780fbe0ea7092c322e04aa3734
achalofficial <achal.official@gmail.com>
2020-03-28 10:45:28 +0530

    Book_OOPS Project

--- Projects/project_OOPBookList/app.js
+++ Projects/project_OOPBookList/app.js
@@ -9,9 +9,47 @@ function Book( title, author, isbn){
 
 function UI() {}
 
+// Add Book to List
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+
+  // Create a tr Element
+  const row = document.createElement('tr');
+  // Insert Cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+
+  UI.prototype.clearField = function () {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+
+  list.appendChild(row);
+}
+
 // Event Listener
 
 document.getElementById('book-form').addEventListener('submit',function (e) {
- const title = document.getElementById('title').value ;
-       author = document.getElementById() 
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value ;
+    
+    // Instantiate Book    
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+    
+    // Add Book to List
+    ui.addBookToList(book);
+
+    // Clear Field
+    ui.clearField();
+
+    e.preventDefault();
 })
