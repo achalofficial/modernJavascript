d955b6090b19546d1b18f019d67c332678fa204b
achalofficial <achal.official@gmail.com>
2020-03-28 10:11:46 +0530

    Book_OOPS Project

---
+++ Projects/project_OOPBookList/app.js
@@ -0,0 +1,17 @@
+// Book Constructor
+function Book( title, author, isbn){
+    this.title = title ;
+    this.author = author ;
+    this.isbn = isbn ;
+}
+
+// UI Constructor
+
+function UI() {}
+
+// Event Listener
+
+document.getElementById('book-form').addEventListener('submit',function (e) {
+ const title = document.getElementById('title').value ;
+       author = document.getElementById() 
+})
