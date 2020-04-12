50024c9af37f9d5ad1ae3285928c06b2600fd55f
achalofficial <achal.official@gmail.com>
2020-04-12 23:08:03 +0530

    Working

--- AsyncJS_API_AJAX/app_3.js
+++ AsyncJS_API_AJAX/app_3.js
@@ -1,1 +1,43 @@
 // The JS Script File
+
+const posts = [
+  {title: 'Post One', body:'This is body one'},
+  {title: 'Post Two', body:'This is body one'}
+];
+
+// function createPost(post){
+//     setTimeout(function(){
+//       posts.push(post);
+//     },2000);
+// }
+
+// function getPost (post){
+//   setTimeout(function(){
+//     let output = '';
+//     posts.forEach(function(post){
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output ;
+//   },1000);
+// }
+
+// createPost({title: 'Post Three', body:'This is Post Three'});
+// getPost();
+
+function createPost(post, callback){
+  setTimeout(function(){
+    posts.push(post);
+  },2000);
+}
+
+function getPost (post){
+setTimeout(function(){
+  let output = '';
+  posts.forEach(function(post){
+    output += `<li>${post.title}</li>`;
+  });
+  document.body.innerHTML = output ;
+},1000);
+}
+
+createPost({title: 'Post Three', body:'This is Post Three'}, getPost);
